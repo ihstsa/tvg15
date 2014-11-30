26daa214f26f14b31491445b2e36574754aa7c49
jacobsilcoff <jacobsilcoff@gmail.com>
2014-11-30 16:22:44 -0500

    Axial Vector And Implementation

--- src/com/ihstsa/tvg15/Tile.java
+++ src/com/ihstsa/tvg15/Tile.java
@@ -1,19 +1,18 @@
 package com.ihstsa.tvg15;
 
-import org.jsfml.graphics.Drawable;
-
-public abstract class Tile implements Drawable
+public abstract class Tile
 {
-	int q, r;
-	HexGrid grid;
-	HexDirections directions;
+	public AxialVector location;
+	private HexGrid grid;
+	
+	public Tile(HexGrid grid, AxialVector point)
+	{
+		this.grid = grid;
+		location = point;
+	}
 	
 	public Tile(HexGrid grid, int q, int r)
 	{
-		directions = new HexDirections(this);
-		this.grid = grid;
-		this.q = q;
-		this.r = r;
+		this(grid, new AxialVector(q, r));
 	}
-	
 }

ac465f9f57a01816f44066d3d3cbcb49587ca13d
jacobsilcoff <jacobsilcoff@gmail.com>
2014-11-30 16:30:23 -0500

    Commented Tile Constructors

--- src/com/ihstsa/tvg15/Tile.java
+++ src/com/ihstsa/tvg15/Tile.java
@@ -5,12 +5,23 @@ public abstract class Tile
 	public AxialVector location;
 	private HexGrid grid;
 	
+	/**
+	 * Makes a Tile
+	 * @param grid The grid the tile will be on.
+	 * @param point The point on which the hexagon lies.
+	 */
 	public Tile(HexGrid grid, AxialVector point)
 	{
 		this.grid = grid;
 		location = point;
 	}
 	
+	/**
+	 * Makes a Tile
+	 * @param grid The grid the tile will be on.
+	 * @param q The q position of the Tile
+	 * @param r The r position of the Tile
+	 */
 	public Tile(HexGrid grid, int q, int r)
 	{
 		this(grid, new AxialVector(q, r));

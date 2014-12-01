7816548b3584ceab5a6e21de4dc6b2ed298d0a70
jacobsilcoff <jacobsilcoff@gmail.com>
2014-11-30 19:59:33 -0500

    getCenterPixelPosition added
    I added a method to Tile to return the centers position as a pixel

--- src/com/ihstsa/tvg15/Tile.java
+++ src/com/ihstsa/tvg15/Tile.java
@@ -1,5 +1,7 @@
 package com.ihstsa.tvg15;
 
+import org.jsfml.system.Vector2i;
+
 public abstract class Tile
 {
 	public static final int SIZE = 50;
@@ -20,11 +22,21 @@ public abstract class Tile
 	/**
 	 * Makes a Tile
 	 * @param grid The grid the tile will be on.
-	 * @param q The q position of the Tile
-	 * @param r The r position of the Tile
+	 * @param q The q position of the Tile.
+	 * @param r The r position of the Tile.
 	 */
 	public Tile(HexGrid grid, int q, int r)
 	{
 		this(grid, new AxialVector(q, r));
+	}
+	
+	/**
+	 * Gets the pixel position of the Tile's center, as a Vector2i.
+	 * @return the pixel position of the Tile's center, as a Vector2i.
+	 */
+	public Vector2i getCenterPixelPosition()
+	{
+		return new Vector2i((int)(Tile.SIZE * 3./2 * pos.q), 
+							(int)(Tile.SIZE * Math.sqrt(3) * (pos.r + pos.q/2)));
 	}
 }
